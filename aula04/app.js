2991420e9575985da63e04fbe706ebaede5da627
aumalves <augusto.malves@mercadolivre.com>
2022-07-25 17:10:13 -0300

    Feat: users routes

--- aula04/app.js
+++ aula04/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const productRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
 app.use(express.json());
 app.use("/products", productRoutes);
+app.use("users", usersRoutes)
 
 app.listen(3001, () => console.log('🚀 app listening on port 3001'))
